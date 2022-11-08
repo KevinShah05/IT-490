147e70070703aca203efb78f816c196ef0199cf7
KevinShah05 <khs4@njit.edu>
2022-11-08 02:10:00 +0000

    Updated ini files

---
+++ RabbitMQ/RabbitMQClient.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function createClientForDb($request){
+  $client = new RabbitMQClient("testRabbitMQ.ini", "testServer");
+  
+  if(isset($argv[1])){
+      $msg = $argv[1];
+  }
+  else{
+      $msg = "client";
+  }
+  
+ 
+  $response = $client->send_request($request);
+  
+  return $response;
+}
+
+
+//  creates rabbitMq client instance for Rabbot MQ server
+
+
+
+
+
+
+?>
