f9d90b21bf6f2c0a857c838fe2186fe5caded2be
Rahee Patel <rap97@njit.edu>
2022-11-10 03:57:17 +0000

    adding frontend

---
+++ Frontend/PHP/RabbitMQClient.php
@@ -0,0 +1,47 @@
+#!/usr/bin/php
+<?php
+
+/*Creating new RabbitMQ Client Instances*/ 
+
+
+session_start();
+require_once('../../RabbitMQ/path.inc');
+require_once('../../RabbitMQ/get_host_info.inc');
+require_once('../../RabbitMQ/rabbitMQLib.inc');
+
+$client = new rabbitMQClient("../../RabbitMQ/testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+
+$userName = $_POST["username"];
+$userPass = $_POST["password"];
+
+$request = array();
+$request['type'] = "login";
+$request['username'] = $userName;
+$request['password'] = $userPass;
+#$request['username'] = "steve";
+#$request['password'] = "password";
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
+if ($response ==1){
+
+	$_SESSION["username"]= $_POST["username"];
+	header("Location:landing.html");
+}else{
+	header("Location:login.html");
+}
+?>
